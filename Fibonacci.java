8e921603aa3f70bb58b1db12bdb72bf3e3d4883f
Malik Azad <113776072+malik-azad@users.noreply.github.com>
2024-09-03 23:46:32 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int terms = sc.nextInt();
+        int n1 = 0, n2 = 1, n3;
+        System.out.print(n1 + " " + n2);
+        for (int i = 2; i < terms; i++) {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
